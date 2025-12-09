032c4135b465b751739b04591532e1e182683e04
chen <1710312281@QQ.COM>
2025-12-09 23:12:26 +0800

    12 9

--- client/src/components/Scanner/styles.ts
+++ client/src/components/Scanner/styles.ts
@@ -15,14 +15,12 @@ export const ScannerContainer = styled.div`
         width: 100%;
         height: 15px;
         border-radius: 20px;
-          /* 扫描渐变核心：从透明→主色→透明，横向渐变 */
         background: linear-gradient(
             90deg,
             rgba(92, 255, 92, 0) 0%,    /* 左侧透明 */
           rgba(92, 255, 92, 0.6) 50%,  /* 中间高亮（原颜色透明度） */
             rgba(92, 255, 92, 0) 100%    /* 右侧透明 */
         );
-         /* 可选：加一层底色，让渐变更明显 */
         background-color: rgba(92, 255, 92, 0.2);
     }
 `
